65df71e1781af4cdf3524c6ffa4fa08b3fbce345
Vaqif <taqiyevv937@gmail.com>
2022-08-23 15:04:47 +0400

    [by_vaqif] problem 4716 solved

---
+++ src/main/kotlin/solutions/lesson_3/by_vaqif/problem_4716.kt
@@ -0,0 +1,12 @@
+package solutions.lesson_3.by_vaqif
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Enter a number of students: ")
+    val student = scan.nextInt()
+    print("Enter a number of an apples: ")
+    val apples = scan.nextInt()
+    print(student / apples)
+}
