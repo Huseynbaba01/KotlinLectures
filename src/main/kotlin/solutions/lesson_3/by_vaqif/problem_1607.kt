c49bf8c203fca5a1bed55ab2ed372e8d2cc29533
Vaqif <taqiyevv937@gmail.com>
2022-08-23 15:02:04 +0400

    [by_vaqif] problem 1607 solved

---
+++ src/main/kotlin/solutions/lesson_3/by_vaqif/problem_1607.kt
@@ -0,0 +1,11 @@
+package solutions.lesson_3.by_vaqif
+
+import java.util.Scanner
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Enter a number: ")
+    val num = scan.nextLong()
+    print(num.toString().reversed())
+
+}
