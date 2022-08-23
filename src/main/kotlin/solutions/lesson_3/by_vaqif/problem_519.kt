e10c08eb4c630a1784fb4339ff8d66e5d94767c4
Vaqif <taqiyevv937@gmail.com>
2022-08-23 14:02:57 +0400

    [by_vaqif] problem 519 solved

---
+++ src/main/kotlin/solutions/lesson_3/by_vaqif/problem_519.kt
@@ -0,0 +1,15 @@
+package solutions.lesson_3.by_vaqif
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Enter a: ")
+    val a = scan.nextInt()
+    print("Enter b: ")
+    val  b = scan.nextInt()
+    print((a*a) + (b*b))
+
+
+
+}
