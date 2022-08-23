03afacd0298547fa7d61a780a01d770e6d3a1b70
Vaqif <taqiyevv937@gmail.com>
2022-08-23 14:01:38 +0400

    [by_vaqif] problem 2 solved

---
+++ src/main/kotlin/solutions/lesson_3/by_vaqif/problem_2.kt
@@ -0,0 +1,10 @@
+package solutions.lesson_3.by_vaqif
+
+import java.util.*
+
+fun main(){
+    val scan = Scanner(System.`in`)
+    print("Enter a number: ")
+    val num = scan.nextInt()
+    println(num.toString().length)
+}
