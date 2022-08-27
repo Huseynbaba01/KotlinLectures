4675d8cd2fc85daec94dc7b5af34fc3e4b9a4bad
dili000 <dilqem9645409@gmail.com>
2022-08-26 23:50:51 -0700

    test yy

---
+++ src/main/kotlin/solutions/lesson6/Circle.kt
@@ -0,0 +1,35 @@
+package solutions.lesson6
+
+import java.lang.Math.sqrt
+import java.util.*
+import java.util.Scanner
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class Circle {
+
+    var R: Double =0.0
+    var D: Double =0.0
+    var S: Double =0.0
+
+    fun rh(){
+        val scanner =Scanner(System.`in`)
+        val  m=scanner.nextInt()
+        for (i in 1..m) {
+            var x1 = scanner.nextDouble()
+            var y1 = scanner.nextDouble()
+            var x2 = scanner.nextDouble()
+            var y2 = scanner.nextDouble()
+            R = sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2))
+            D = 2 * R
+            S = Math.PI * R.pow(2)
+            print("Radius= = " + R + " Diametr = " + D + " Sahə = " + S)
+        }
+    }
+
+}
+
+fun main() {
+
+
+}
