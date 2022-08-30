d80ff38072ade96b510de3e87658ebd25f0e03c7
Aynura Ibrahimova <Aynura.Ibrahimova@kapitalbank.az>
2022-08-31 00:47:16 +0400

    class example

--- src/main/kotlin/solutions/lesson6/test.kt
+++ src/main/kotlin/solutions/lesson6/test.kt
@@ -2,7 +2,7 @@ package solutions.lesson6
 
 import kotlin.math.pow
 
-class test {
+class Lesson6Task {
     var n: Int?=null
     var x1: Double?=null
     var x2: Double?=null
@@ -24,13 +24,13 @@ class test {
         r = Math.sqrt(Math.pow((x2 - x1), 2.0) + Math.pow((y2 - y1), 2.0))
         d = 2 * r
         area = java.lang.Math.PI * r.pow(2)
-        println(String.format("%.1f ,%.1f ,%.1f " ,r,d,area))
+        println(String.format("%.2f ,%.2f ,%.2f " ,r,d,area))
     }
 }
     fun main() {
 
-        var circle1 = test(2,0.3,0.5,1.2,-4.0)
-        var circle2=test(2 ,0.0, 0.0 ,3.0, 4.0 )
+        var circle1 = Lesson6Task(2,0.3,0.5,1.2,-4.0)
+        var circle2=Lesson6Task(2 ,0.0, 0.0 ,3.0, 4.0 )
         circle1.getCircle(2,0.3,0.5,1.2,-4.0)
         circle2.getCircle(2,0.0, 0.0 ,3.0 ,4.0 )
 
---
+++ src/main/kotlin/solutions/lesson_7.kt
@@ -0,0 +1,15 @@
+package solutions
+
+class lesson_7 {
+    fun turnOn():Boolean{
+        return true
+    }
+fun turnOff():Boolean{
+    return false
+}
+}
+fun main(){
+    val dv=lesson_7()
+   println( dv.turnOff())
+    println( dv.turnOn())
+}
